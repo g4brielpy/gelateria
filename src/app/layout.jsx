5ef7bc6eaab10d913805b4595a21337a40b736dc
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-07-11 18:40:53 -0300

    Reset next

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Gelateria",
+  description: "Landing Page Sorveteria criado com Next, React e Tailwindcss",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
