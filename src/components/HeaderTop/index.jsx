6b831ea1f1699fb77d1b5c4e64a4a010dadcb2db
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-07-14 11:21:32 -0300

    Component HeaderTop

--- src/components/HeaderTop/index.jsx
+++ src/components/HeaderTop/index.jsx
@@ -1,23 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "/public/logo.png";
 
 export default function Header() {
   return (
-    <header
-      className="
-      container 
-      flex 
-        items-center justify-between"
-    >
-      <div>
-        <Image src="/logo.png" alt="Logo Sorveteria" width="90" height="100" />
+    <header className="bg-emphasis">
+      <div
+        className="
+        container
+        flex 
+          items-center justify-between flex-wrap gap-4"
+      >
+        <div>
+          <Image
+            src="/logo.png"
+            alt="Logo Sorveteria"
+            width="100"
+            height="100"
+            className="object-cover"
+          />
+        </div>
+        <nav className="space-x-6 font-bold">
+          <Link href="/">Home</Link>
+          <Link href="/">Sabores</Link>
+          <Link href="/">Sobre</Link>
+        </nav>
       </div>
-      <nav className="space-x-6 font-bold">
-        <Link href="/">Home</Link>
-        <Link href="/">Sabores</Link>
-        <Link href="/">Sobre</Link>
-      </nav>
     </header>
   );
 }
