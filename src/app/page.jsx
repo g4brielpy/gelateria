3f3703b872b50cce341eb72905aabf6dd2fca286
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-07-14 11:43:23 -0300

    Banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <h1>Ola mundo </h1>;
+  return (
+    <>
+      <section
+        className="
+          w-full h-screen relative
+          bg-no-repeat bg-center bg-auto bg-emphasis
+          flex items-center justify-center
+        "
+        style={{ backgroundImage: "url('/banner-home.png')" }}
+      >
+        <h1 className="text-white font-extrabold text-7xl drop-shadow-lg">
+          Sorvete Artesanal
+        </h1>
+      </section>
+      <main>
+        <h2>main</h2>
+      </main>
+    </>
+  );
 }
