f79ed63e40ea0d3789d051a4531965b459d76ee0
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-07-15 16:14:32 -0300

    Att cardsHome

--- src/components/CardsHome/index.jsx
+++ src/components/CardsHome/index.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export function CardsHome({
+export default function CardsHome({
   titulo = "",
   subTitulo = "",
   sobre = "",
@@ -9,9 +9,17 @@ export function CardsHome({
 }) {
   return (
     <div
-      className={`flex flex-row flex-wrap ${isReverse && "flex-row-reverse"}`}
+      className={`
+        flex flex-row flex-wrap 
+        ${isReverse && "flex-row-reverse"}
+      `}
     >
-      <div className="image bg-red-600 flex-auto min-w-[500px] w-1/2 h-[400px]">
+      <div
+        className="
+          min-w-[500px] w-1/2 h-[400px]
+          flex-auto
+          "
+      >
         <Image
           src={img.src}
           alt={img.alt}
@@ -22,48 +30,17 @@ export function CardsHome({
       </div>
       <div
         className="
-          min-w-[500px] w-1/2 h-[400px] text-center flex-auto flex
-          flex-col justify-center items-center
+          min-w-[500px] w-1/2 h-[400px] 
+          text-center flex-auto flex
+            flex-col justify-center items-center
           "
       >
-        <h2 className="text-3xl font-bold mb-6">{titulo}</h2>
+        <h2 className="text-3xl font-bold mb-6 drop-shadow-md">{titulo}</h2>
         <h3 className="text-lg font-semibold italic mb-4">{subTitulo}</h3>
         <p className="text-sm font-light text-gray-900 leading-6 max-w-[500px]">
           {sobre}
         </p>
       </div>
     </div>
-
-    // <div
-    //   className={`flex flex-col md:flex-row ${
-    //     isReverse && "md:flex-row-reverse"
-    //   }`}
-    // >
-    //   <div
-    //     className="
-    //     p-4 bg-slate-400
-    //     flex flex-1 text-center
-    //       flex-col justify-center items-center
-    //   "
-    //   >
-    //     <h2 className="text-3xl font-bold uppercase mb-5">{titulo}</h2>
-    //     <h3 className="font-semibold italic mb-4 text-gray-900">{subTitulo}</h3>
-    //     <p
-    //       className="text-sm text-gray-800"
-    //       style={{ maxWidth: "500px", lineHeight: "1.6rem" }}
-    //     >
-    //       {sobre}
-    //     </p>
-    //   </div>
-    //   <div className="flex-1">
-    //     <Image
-    //       src={img.src}
-    //       alt={img.alt}
-    //       width="500"
-    //       height="500"
-    //       className="w-full h-full object-cover"
-    //     />
-    //   </div>
-    // </div>
   );
 }
