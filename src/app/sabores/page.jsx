809ff4fd3135a573c6da983b9656b2881b4758db
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-07-19 13:33:37 -0300

    Add banner Sabores

--- src/app/sabores/page.jsx
+++ src/app/sabores/page.jsx
@@ -1,5 +1,20 @@
 import Image from "next/image";
 
 export default function page() {
-  return <div></div>;
+  return (
+    <>
+      <section
+        className="
+          w-full h-screen
+          bg-no-repeat bg-center bg-cover
+          bg-[url('/banner-sabores.jpg')]
+          flex items-center justify-center
+        "
+      >
+        <h1 className="text-white text-center font-extrabold text-5xl md:text-7xl drop-shadow-lg px-4">
+          Sorvete Artesanal
+        </h1>
+      </section>
+    </>
+  );
 }
