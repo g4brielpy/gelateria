228f1caca94d3de28e0070b4bdf4777e478c4648
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-07-19 18:09:16 -0300

    /

--- src/app/sabores/page.jsx
+++ src/app/sabores/page.jsx
@@ -12,15 +12,30 @@ export default function page() {
           flex items-center justify-center
         "
       >
-        <h1 className="text-white text-center font-extrabold text-5xl md:text-7xl drop-shadow-lg px-4">
-          Sorvete Artesanal
+        <h1
+          className="
+            text-white text-center font-extrabold drop-shadow-lg px-4
+            text-5xl md:text-7xl
+            "
+        >
+          Nossos Sorvetes
         </h1>
       </section>
-      <main className="container py-12">
-        <h2 className="uppercase text-center text-2xl sm:text-3xl md:text-4xl font-semibold">
+      <main className="container py-16">
+        <h2
+          className="
+            uppercase text-center font-semibold mb-10
+            text-2xl sm:text-3xl md:text-4xl
+            "
+        >
           Sabores de Sorvete
         </h2>
-        <div>
+        <div className="flex flex-col flex-wrap md:flex-row gap-4">
+          <CardSorvete />
+          <CardSorvete />
+          <CardSorvete />
+          <CardSorvete />
+          <CardSorvete />
           <CardSorvete />
         </div>
       </main>
--- src/components/CardSorvete/index.jsx
+++ src/components/CardSorvete/index.jsx
@@ -2,19 +2,34 @@ import Image from "next/image";
 
 export default function CardSorvete() {
   return (
-    <div className="bg-slate-200 w-[300px] p-4 text-center flex flex-col items-center">
-      <div className="image bg-slate-400 min-h-[200px] flex">
+    <div
+      className="
+          bg-gray-100/5 rounded-md shadow-md
+          w-[300px] p-4 text-center
+          flex-auto flex 
+            flex-col items-center"
+    >
+      <div className="h-[300px] flex justify-center">
         <Image
           src="/sabor-oreo.png"
           alt="Image sabor Oreo"
           width="500"
           height="200"
-          className="object-cover w-fit"
+          className="object-cover flex-auto"
         />
       </div>
-      <div className="image bg-slate-00 p">
-        <h3 className="font-bold">Sorvete de Oreo</h3>
-        <small>Delicioso sorvete sabor Oreo. Uma explosão de sabor.</small>
+      <div className="image bg-slate-00 py-4">
+        <h3
+          className="
+            font-bold mb-2 md:text-xl pt-4
+            border-t-2 border-solid border-gray-400/40
+            "
+        >
+          Sorvete de Oreo
+        </h3>
+        <small className="md:text-sm text-gray-900">
+          Delicioso sorvete sabor Oreo. Uma explosão de sabor.
+        </small>
       </div>
     </div>
   );
